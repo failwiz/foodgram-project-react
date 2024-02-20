86c9537863f60428b908bc69651d2f1c616e86c2
Alexey Turovskiy <a.turowskiy@gmail.com>
2024-02-20 16:03:45 +0700

    Add base admin models for recipes.models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import (
+        Ingredient,
+        IngredientAmount,
+        Tag,
+        Recipe,
+        Unit,
+)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'author',
+        'description',
+        'ingredients',
+        'tags',
+        'time_to_cook',
+    )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'color',
+        'slug',
+    )
+
+
+@admin.register(IngredientAmount)
+class IngredientAmountAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'recipe',
+        'ingredient',
+        'unit',
+        'amount',
+    )
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'short_name',
+        'base_unit',
+        'cooef',
+    )
